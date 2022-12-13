7c56326de451fccee7778374c09a3ce255e48e9c
Valéria Beserra Garcia <vb.garcia@sidi.org.br>
2022-12-13 11:00:07 -0300

    Change the app theme and Change the app icon

--- app/src/main/java/com/valeria/tiptime/MainActivity.kt
+++ app/src/main/java/com/valeria/tiptime/MainActivity.kt
@@ -16,38 +16,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateButton.setOnClickListener { calculateTip() }
+//        binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip() {
-
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-
-        if (cost == null) {
-            displayTip(0.0)
-            return
-        }
-
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
-        }
-
-        var tip = tipPercentage * cost
-        if(binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
-        }
-
-        displayTip(tip)
-
-    }
-
-    private fun displayTip(tip : Double) {
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-    }
+//    fun calculateTip() {
+//
+//        val stringInTextField = binding.costOfService.text.toString()
+//        val cost = stringInTextField.toDouble()
+//
+//        if (cost == null) {
+//            displayTip(0.0)
+//            return
+//        }
+//
+//        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+//            R.id.option_twenty_percent -> 0.20
+//            R.id.option_eighteen_percent -> 0.18
+//            else -> 0.15
+//        }
+//
+//        var tip = tipPercentage * cost
+//        if(binding.roundUpSwitch.isChecked) {
+//            tip = kotlin.math.ceil(tip)
+//        }
+//
+//        displayTip(tip)
+//
+//    }
+//
+//    private fun displayTip(tip : Double) {
+//        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+//        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+//    }
 
 
 }
